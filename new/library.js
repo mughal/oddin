213641f91d61b56ea6d80b8686597b459f222d55
mughal <mughal@gmail.com>
2024-06-02 22:03:04 +0500

    Learning grid

--- new/library.js
+++ new/library.js
@@ -73,4 +73,29 @@ btn.addEventListener("click", function (e) {
   });
   }
 
+
+  const showButton = document.getElementById("showDialog");
+  const bookDialog = document.getElementById("bookDialog");
+  const confirmBtn = bookDialog.querySelector("#confirmBtn");
+  
+  // "Show the dialog" button opens the <dialog> modally
+  showButton.addEventListener("click", () => {
+    bookDialog.showModal();
+  });
+  
+ 
+  // "Cancel" button closes the dialog without submitting because of [formmethod="dialog"], triggering a close event.
+  bookDialog.addEventListener("close", (e) => {
+    outputBox.value =
+    bookDialog.returnValue === "default"
+        ? "No return value."
+        : `ReturnValue: ${bookDialog.returnValue}.`; // Have to check for "default" rather than empty string
+  });
+  
+  // Prevent the "confirm" button from the default behavior of submitting the form, and close the dialog with the `close()` method, which triggers the "close" event.
+  confirmBtn.addEventListener("click", (event) => {
+    event.preventDefault(); // We don't want to submit this fake form
+    bookDialog.close(); // Have to send the select box value here.
+  });
+  
   displayLibrary();
